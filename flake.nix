52cdb437a912491e8c356e45afefec3904af8d8b
SpinEch0 <vivazsj@gmail.com>
2024-12-03 16:27:53 +0800

    add nix env

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "opengpu environment";
+
+
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }@inputs:
+    let
+      overlay = import ./overlay.nix;
+    in
+    flake-utils.lib.eachDefaultSystem
+      (system:
+      let
+        pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; };
+        deps = with pkgs; [
+          git
+          gnumake autoconf automake
+          mill
+          dtc
+          verilator cmake ninja
+          circt
+        ];
+      in
+        {
+          legacyPackages = pkgs;
+          devShell = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+            buildInputs = deps;
+            shellHook = ''
+              echo "Hello opengpu world!"
+            '';
+          };
+        }
+      )
+    // { inherit inputs; overlays.default = overlay; };
+}
