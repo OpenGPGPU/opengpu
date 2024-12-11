ead19cc4c9f15cdd4b415e3abd54474e0ed90fc0
a@a.com <a@a.com>
2024-12-11 21:54:51 +0800

    update mill version

--- overlay.nix
+++ overlay.nix
@@ -1,9 +1,9 @@
 final: prev: {
   mill = prev.mill.overrideAttrs (oldAttrs: rec {
-    version = "0.12.3";
+    version = "0.11.13";
     src = prev.fetchurl {
       url = "https://github.com/com-lihaoyi/mill/releases/download/${version}/${version}-assembly";
-      sha256 = "sha256-hqzAuYadCciYPs/b6zloLUfrWF4rRtlBSMxSj7tLg7g=";
+      sha256 = "sha256-hfhxIAEpl7dafW4+FGBHZfeTdQ7Dreou9r/6Kstai8A=";
     };
   });
 }
