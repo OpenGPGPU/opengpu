a3b67ea5b98aec447a6672127c86f31062d269ac
DonaldDuck <vivazsj@gmail.com>
2025-01-03 18:23:31 +0800

    CuTaskBundle

--- src/core/Bundle.scala
+++ src/core/Bundle.scala
@@ -7,5 +7,10 @@ class CuTaskBundle(
   threadNum: Int,
   warpNum:   Int,
   dimNum:    Int,
-  xLen:      Int)
-    extends Bundle {}
+  xLen:      Int,
+  dimWidth:  Int = 16)
+    extends Bundle {
+  val mask = Vec(threadNum, Bool())
+  val dim = Vec(dimNum, UInt(dimWidth.W))
+  val pc = UInt(32.W)
+}
